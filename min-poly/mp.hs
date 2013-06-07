16c9a192ece6917331eac5e100cc4e4dcfe39712
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2013-06-07 18:03:26 -0400

    WIP

--- min-poly/mp.hs
+++ min-poly/mp.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 
 data Poly a = Poly [a] deriving (Show, Eq)
 
@@ -18,6 +19,9 @@ poly_mul a (Poly (b:bs)) =
   poly_add (poly_c b a) (poly_x (poly_mul a (Poly bs)))
   where poly_x (Poly a) = Poly (0:a)
 
+plift f (Poly p) = Poly $ f p
+plist f (Poly p) = f p
+
 instance (Num a) => (Num (Poly a)) where
   (+) = poly_add
   (-) = poly_sub
@@ -26,6 +30,26 @@ instance (Num a) => (Num (Poly a)) where
   abs = undefined
   signum = undefined
   
-  
+trim (Poly p) = if (last p == 0) then trim $ Poly $ init p else Poly p
+degree :: Num a => Poly a -> Int
+degree = plist length . trim
+leading :: Num a => Poly a -> a
+leading = plist last . trim
 
+-- ----------------------------------------------------------------  
+-- next task: polynomials under some relation
+-- for example, with relator [x^2 - 2] the polynomial x^2 should be
+-- reduced to 2.
   
+data Relator p where
+  Rel :: Poly p -> Relator p
+
+liftrel f (Rel p) = f p
+-- evaluate a polynomial in some constant x for which we know a relator
+-- or, reduce poloynomial p, mod rel
+relmod rel p = 
+  if degree p < liftrel degree rel then p
+  else
+    relmod rel p'
+    where f = leading p / liftrel leading rel
+          p' = trim $ p - liftrel (poly_c f) rel
