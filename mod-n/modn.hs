b958e13fe71b58643df3d5ede26c22f5ced03147
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2013-06-08 18:08:18 -0400

    module-ize modn.hs

--- mod-n/modn.hs
+++ mod-n/modn.hs
@@ -1,3 +1,4 @@
+module ModN (mod2,mod3,mod4,mod5,mod6,mod7) where
 
 data Zero
 data Succ a
@@ -14,13 +15,21 @@ instance Modulus Zero where
 instance Modulus a => Modulus (Succ a) where
   modulus = \a -> 1 + modulus (lower a)
 
-m7 = undefined :: Succ (Succ (Succ (Succ (Succ (Succ (Succ Zero))))))
+m2 = undefined :: Succ (Succ Zero)
+m3 = undefined :: Succ (Succ (Succ Zero))
+m4 = undefined :: Succ (Succ (Succ (Succ Zero)))
+m5 = undefined :: Succ (Succ (Succ (Succ (Succ Zero))))
 m6 = undefined :: Succ (Succ (Succ (Succ (Succ (Succ Zero)))))
+m7 = undefined :: Succ (Succ (Succ (Succ (Succ (Succ (Succ Zero))))))
 
 data ModN m n = ModN (m,n)
 
-mod7 n = ModN  (m7, n)
+mod2 n = ModN  (m2, n)
+mod3 n = ModN  (m3, n)
+mod4 n = ModN  (m4, n)
+mod5 n = ModN  (m5, n)
 mod6 n = ModN  (m6, n)
+mod7 n = ModN  (m7, n)
 
 instance (Modulus m, Integral n) => Show (ModN m n) where
   show (ModN (m, n)) = show (n `mod` modulus m) ++
